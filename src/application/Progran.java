c9975eb061bee85b42d34fb37f96f2335e672f5e
Francielxyz <ruanvha15@gmail.com>
2021-01-12 21:20:01 -0300

    ChessMatch-Partida e ChessPiece-Peça

--- src/application/Progran.java
+++ src/application/Progran.java
@@ -1,11 +1,13 @@
 package application;
 
+import chess.ChessMatch;
 
 public class Progran {
 
 	public static void main(String[] args) {
 		
-		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -30,6 +30,13 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	public Piece piece(int rows, int columns) {
+		return pieces[rows][columns];
+	}
 	
+	//Sobrecarga
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 
 }
