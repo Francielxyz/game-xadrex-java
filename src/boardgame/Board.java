5f0b9588e8d6f7b5e61e77852e057e75c9bafbd8
Francielxyz <ruanvha15@gmail.com>
2021-01-14 23:20:44 -0300

    Placing/colocado pieces on the board/tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -34,9 +34,14 @@ public class Board {
 		return pieces[rows][columns];
 	}
 	
-	//Sobrecarga
+	//Sobrecarga da classe Peça
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
 	}
 
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rook;
+import chess.pieces.King;
 
 //Partida de Xadrez
 public class ChessMatch {
@@ -9,6 +12,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 
 	public ChessPiece[][] getPieces(){
@@ -20,4 +24,12 @@ public class ChessMatch {
 			}
 			return mat; //Retorna a matrix de peças da partida
 	}
+	
+	//Método responsável por iniciar a partida de Xadrez
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLAKC), new Position(0, 4));
+		board.placePiece(new Rook(board, Color.BLAKC), new Position(7, 3));
+	}
+	
 }
