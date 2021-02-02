1bae225830291943562fdeb33fd558ec41d88668
Francielxyz <ruanvha15@gmail.com>
2021-02-02 15:08:48 -0300

    Impresão de tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,33 +6,41 @@ import chess.pieces.Rook;
 
 //Partida de Xadrez
 public class ChessMatch {
-	
+
 	private Board board;
-	
+
 	public ChessMatch() {
 		board = new Board(8, 8);
 		initialSetup();
 	}
 
-	public ChessPiece[][] getPieces(){
-			ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-			for(int i = 0; i < board.getRows(); i++) {
-				for(int j = 0; j < board.getColumns(); j++) {
-					mat[i][j] = (ChessPiece) board.piece(i, j);
-				}
+	public ChessPiece[][] getPieces() {
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
-			return mat; //Retorna a matrix de peças da partida
+		}
+		return mat;
 	}
-	
-	private void placeNewPiece (char column, int row, ChessPiece piece) {
+
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
-	//Método responsável por iniciar a partida de Xadrez
+
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
 		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+		placeNewPiece('c', 7, new Rook(board, Color.BLAKC));
+		placeNewPiece('c', 8, new Rook(board, Color.BLAKC));
+		placeNewPiece('d', 7, new Rook(board, Color.BLAKC));
+		placeNewPiece('e', 7, new Rook(board, Color.BLAKC));
+		placeNewPiece('e', 8, new Rook(board, Color.BLAKC));
+		placeNewPiece('d', 8, new King(board, Color.BLAKC));
 	}
-	
 }
